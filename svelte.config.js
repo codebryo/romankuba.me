713b5f00cb34b2ad05e49db520c24314dab59283
Roman Kuba <roman.kuba@gmail.com>
2024-12-02 09:03:51 +0100

    Updated netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import { mdsvex } from "mdsvex";
-import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,13 +9,13 @@ const config = {
     preprocess: [vitePreprocess(), mdsvex()],
 
     kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
+        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+        adapter: adapter(),
+    },
 
-    extensions: [".svelte", ".svx"]
+    extensions: ['.svelte', '.svx'],
 };
 
 export default config;
