4dea30c86c6a3e2531306d96fba6ec00f923c478
Roman Kuba <roman.kuba@gmail.com>
2025-02-02 07:47:41 +0100

    Added og tags to post

--- src/routes/journal/[slug]/+page.svelte
+++ src/routes/journal/[slug]/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import { formatDate } from '$lib/utils';
+	import { page } from '$app/state';
+
+	const ogPath = `${page.url.origin}/og/${page.url.pathname.replace('/journal/', '')}.png`;
 
 	let { data } = $props();
 </script>
@@ -8,15 +11,16 @@
 	<title>{data.meta.title}</title>
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={data.meta.title} />
+	<meta property="og:description" content={data.meta.description} />
+	<meta property="og:url" content={page.url.href} />
+	<meta property="og:image" content={ogPath} />
+	<meta property="og:image:type" content="image/png" />
 </svelte:head>
 
 <article class="mt-16">
 	<hgroup class="mb-8">
 		<h1>{data.meta.title}</h1>
-		<p
-			class="text-sm text-slate-500 italic
-		"
-		>
+		<p class="text-sm text-slate-500 italic">
 			Published at {formatDate(data.meta.date)}
 		</p>
 	</hgroup>
